f928a57b53805e64628a55dad1d625520b1d1e4e
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-07-03 11:54:49 -0500

    Implementacion de un Estado Global

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,25 @@
 <script>
+	import { globalState } from '../store/weather.global.js';
 	import { getWeatherFrom } from '../services/weather.js';
+	import WeatherSearch from '../components/weather.search.svelte';
 	import WeatherFooter from '../components/weather.footer.svelte';
-	import WeatherIcon from '../components/weather.icon.svelte';
-	const weatherPromise = getWeatherFrom();
+	import WeatherContent from '../components/weather.content.svelte';
+
+	let weatherPromise = {};
+
+	// Global State - Weather Controller Search
+	globalState.set(getWeatherFrom());
+	globalState.subscribe((value) => {
+		weatherPromise = value;
+	});
 </script>
 
 {#await weatherPromise then weather}
-	<section>
-		<h1>{weather.locationName}</h1>
-		<h2>{weather.temperature}°</h2>
-		<h3>{weather.conditionText}</h3>
-		<WeatherIcon icon={weather.conditionIcon} text={weather.conditionText} />
-		<h4>{weather.windSpeed}</h4>
-	</section>
-	<WeatherFooter />
+	<WeatherSearch />
+	<WeatherContent {weather} />
+	<WeatherFooter
+		humidity={weather.humidity}
+		windSpeed={weather.windSpeed}
+		feelsLike={weather.feelsLike}
+	/>
 {/await}
-
-<style>
-	section {
-		padding: 32px 0 16px 16px;
-	}
-	h1 {
-		font-weight: 400;
-		color: #333;
-		text-transform: uppercase;
-	}
-	h2 {
-		font-weight: 400;
-		font-size: 74px;
-		color: #333;
-		text-transform: uppercase;
-	}
-	h3 {
-		font-weight: 500;
-		transform: rotate(-90deg);
-		position: absolute;
-		top: 80px;
-		right: 0px;
-	}
-</style>
