3140b9a67e22b049c993ddbfad8bac9573a0a6a2
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-07-03 12:03:29 -0500

    TailwindCSS

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,10 @@
-<div>
+<script>
+	import '../app.css';
+</script>
+
+<div
+	class="h-[600px] w-[400px] max-w-full block shadow-md rounded-lg border relative pl-3 pr-3 pt-3"
+>
 	<slot />
 </div>
 
@@ -7,28 +13,16 @@
 
 	:global(body) {
 		background: #fdfdfd;
-		display: grid;
 		font-family: 'Gordita', sans-serif;
-		place-content: center;
-		min-height: 100vh;
-		margin: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
 	}
 
 	:global(*, *::after, *::before) {
-		box-sizing: border-box;
-	}
-
-	:global(h1, h2, h3, h4, h5) {
+		padding: 0;
 		margin: 0;
-	}
-
-	div {
-		aspect-ratio: 9 / 16;
-		width: 400px;
-		max-width: 100%;
-		display: block;
-		box-shadow: 0 0 100px rgba(0, 0, 0, 0.1);
-		border-radius: 8px;
-		position: relative;
+		box-sizing: border-box;
 	}
 </style>
